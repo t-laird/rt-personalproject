bfa17cd941c1fa1599f07d898279db54e4398665
Thomas Laird <t6r6l5@gmail.com>
2017-12-21 16:39:25 -0700

    add navlink for authrocket

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,12 +5,13 @@ import './Header.css'
 const Header = () => {
 
 	return (
-		<div className="header-component">
-			<NavLink exact to='/'><h1>APP NAME</h1></NavLink>
-			<NavLink className="login" to='/login'>LOGIN</NavLink>
-			<NavLink className="user" to='/user'>USER</NavLink>
-			<NavLink className="group" to='/group'>GROUP</NavLink>
-		</div>
+  <div className="header-component">
+    <NavLink exact to='/'><h1>APP NAME</h1></NavLink>
+    <a className="login" href="https://tr-personal-proj.e1.loginrocket.com">LOGIN/SIGNUP</a>
+    {/* <NavLink className="login" to='/login'>LOGIN</NavLink> */}
+    <NavLink className="user" to='/user'>USER</NavLink>
+    <NavLink className="group" to='/group'>GROUP</NavLink>
+  </div>
 	)
 }
 
