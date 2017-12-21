cea52bca6f2d9228ac8a0b43454f1466da87b076
Thomas Laird <t6r6l5@gmail.com>
2017-12-21 16:41:03 -0700

    add user reducer

---
+++ src/Reducers/User.js
@@ -0,0 +1,10 @@
+const UserReducer = (state = {}, action) => {
+  switch(action.type) {
+    case "UPDATE_USER":
+      return action.user;
+    default:
+      return state;
+  }
+}
+
+export default UserReducer;
