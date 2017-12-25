7e3a9168a1f052db951efe66f9717e1f007c8196
Thomas Laird <t6r6l5@gmail.com>
2017-12-24 20:50:40 -0700

    reducer for user transactions

---
+++ src/Reducers/UserTransactions.js
@@ -0,0 +1,8 @@
+const UserTransactions = (store = [], action) {
+  switch(action.type) {
+    case 'UPDATE_TRANSACTIONS':
+      return action.transactions;
+    default: 
+      return store;
+  }
+}
