63177e26e1a81b5add6d07c03c682fafc56024ca
Thomas Laird <t6r6l5@gmail.com>
2017-12-25 20:42:03 -0700

    reducers for getting userlist and user transactions

---
+++ src/Reducers/UserList.js
@@ -0,0 +1,10 @@
+const UserList = (store = [], action) => {
+  switch(action.type) {
+    case 'UPDATE_USERS':
+      return action.users;
+    default: 
+      return store;
+  }
+}
+
+export default UserList;
