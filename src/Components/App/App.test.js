799db5a2346a07947d9e8c3553c3773a890579bb
Thomas Laird <t6r6l5@gmail.com>
2017-12-26 19:26:07 -0700

    app tests

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+
+describe('app tests', () => {
+  let renderedApp;
+  beforeEach(() => {
+    renderedApp = shallow(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+  })
+  
+  it('renders without crashing', () => {
+    expect(renderedApp).toBeDefined();
+  });
+
+  it('should render with the correct default elements', () => {
+    expect(renderedApp.find('App').length).toEqual(1);
+  });
+
+})
