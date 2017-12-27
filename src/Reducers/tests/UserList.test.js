fb62d58a80d5c44a11aa1ef926a5596d9b120110
Thomas Laird <t6r6l5@gmail.com>
2017-12-26 19:28:05 -0700

    userlist reducer tests

---
+++ src/Reducers/tests/UserList.test.js
@@ -0,0 +1,30 @@
+import UserList from '../UserList';
+import * as actions from '../../Actions';
+
+describe('UserList reducer tests', () => {
+  it('returns the correct default state', () => {
+    const expectedState = [];
+
+    expect(UserList(undefined, {})).toEqual(expectedState);
+  });
+
+  it('should return the current state when passed a non-existant action', () => {
+    const mockState = [
+      {name: 'Michelle'}, {name: 'George'}
+    ];
+
+    const mockAction = {
+      type: 'FAKE_ACTION'
+    };
+
+    expect(UserList(mockState, mockAction)).toEqual(mockState);
+  });
+
+  it('should update the user list when given the UPDATE_USERS action', () => {
+    const mockUsers = [
+      {name: 'Michelle'}, {name: 'George'}
+    ];
+
+    expect(UserList([], actions.updateUserList(mockUsers))).toEqual(mockUsers);
+  });
+});
