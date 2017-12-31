b1abf87d09daac2eb67e6c4e6d7980ac5a93dfae
Rob Morgan <rmorgan323@gmail.com>
2017-12-31 14:28:23 -0700

    join group component conditional rendering and styling

--- src/Components/JoinGroup/JoinGroup.js
+++ src/Components/JoinGroup/JoinGroup.js
@@ -3,71 +3,75 @@ import { connect } from 'react-redux';
 import * as actions from '../../Actions';
 import { NavLink } from 'react-router-dom';
 import validateGroup from '../../helpers/validateGroup';
+import './JoinGroup.css';
 
 
 
 class JoinGroup extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			passphrase: '',
-			message: `You haven't joined a group yet.  And you should!  Enter your group passphrase below:`
-		}
-	}
+    this.state = {
+      passphrase: '',
+      message: `You haven't joined a group yet.  And you should!  Enter your group passphrase below:`
+    }
+  }
 
-	componentDidMount = () => {
-		if (this.props.group.group_name) {
-			this.setState({message: `You are a member of: ${this.props.group.group_name}`})
-		} else if (this.props.user.group_id) {
-			this.setState({message: 'You are a member of a group!'})
-		}
-	}
+  componentDidMount = () => {
+    if (this.props.group.group_name) {
+      this.setState({message: `You are a member of: ${this.props.group.group_name}.  To switch groups, enter new passphrase below.`})
+    } else if (this.props.user.group_id) {
+      this.setState({message: 'You are a member of a group!  To switch groups, enter new passphrase below.'})
+    }
+  }
 
-	handleChange = (event) => {
-		const { target } = event;
-		const { name, value } = target;
+  handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
 
-		this.setState({[name]: value})
-	}
+    this.setState({[name]: value})
+  }
 
-	joinGroup = async (event) => {
-		event.preventDefault();
-		const response = await validateGroup(this.state.passphrase, this.props.user.user_id)
-		this.props.updateUser(response[0])
-	}
+  joinGroup = async (event) => {
+    event.preventDefault();
+    const response = await validateGroup(this.state.passphrase, this.props.user.user_id)
+    this.props.updateUser(response[0])
+  }
 
-	render() {
-		return (
-			<div>
-				<h4>{this.state.message}</h4>
-				<form>
-					<input 
-						value={this.state.passphrase}
-						onChange={this.handleChange}
-						placeholder='passphrase' 
-						name='passphrase' />
-					<button
-						onClick={this.joinGroup}
-					>submit</button>
-				</form>
+  render() {
+    return (
+      <div className="join-group-component">
+        <div className="join-container">
+          <h3>{this.state.message}</h3>
+          <form>
+            <input 
+              value={this.state.passphrase}
+              onChange={this.handleChange}
+              placeholder='passphrase' 
+              name='passphrase' />
+            <button
+              onClick={this.joinGroup}
+            >SUBMIT</button>
+          </form>
+        </div>
 
-				<NavLink to='/creategroup'>Create Group</NavLink>
-
-			</div>
-		) 
-	}
+        <div className="link-holder">
+          <NavLink className="create-group-link" to='/creategroup'>CREATE NEW GROUP</NavLink>
+        </div>
+      </div>
+    ) 
+  }
 }
 
 const mapStateToProps = ( store ) => ({
-	user: store.User,
-	group: store.Group
+  user: store.User,
+  group: store.Group
 })
 
 const mapDispatchToProps = dispatch => ({
-	updateUser: user => {
-		dispatch(actions.updateUser(user))
-	}
+  updateUser: user => {
+    dispatch(actions.updateUser(user))
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(JoinGroup);
