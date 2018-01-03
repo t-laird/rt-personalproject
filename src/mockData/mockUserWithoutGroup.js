b238fc941f5f0f3cff218c739974d4f2981489a8
Rob Morgan <rmorgan323@gmail.com>
2018-01-03 11:11:27 -0700

    add user tests

---
+++ src/mockData/mockUserWithoutGroup.js
@@ -0,0 +1,8 @@
+export const mockUserWithoutGroup = {
+  authrocket_id: "usr_0vXWX9jSRkbUjLGsOb9S8r",
+  created_date: "2017-11-18T18:34:30.017Z",
+  email: "cpavin@usopen.com",
+  group_id: null,
+  name: "Corey Pavin",
+  user_id: 95
+};
