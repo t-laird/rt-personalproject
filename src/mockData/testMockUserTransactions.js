212eba3385e70b01575791098ba7d3ea1759bb51
Thomas Laird <t6r6l5@gmail.com>
2018-01-03 10:59:25 -0700

    create mock transactions w/o date.now for snapshots

---
+++ src/mockData/testMockUserTransactions.js
@@ -0,0 +1,44 @@
+const mockUserTransactions = [
+  {
+    received: [
+      {
+        'send_id': 5,
+        'receive_id': 8,
+        'group_id': 1,
+        'created_time': 1504239488330,    
+        'point_value': 10,
+        'send_name': 'Ronald Marshall',
+        'note': 'Tacos make me happy.'
+      },
+      {
+        'send_id': 1,
+        'receive_id': 8,
+        'group_id': 1,
+        'created_time': 1504239487190,
+        'point_value': 10,
+        'send_name': 'Paavo Marsicek',
+        'note': 'I did not think we would ever get finished with testing.'  
+      }
+    ],
+    sent: [
+      {
+        'send_id': 8,
+        'receive_id': 4,
+        'group_id': 1,
+        'created_time': 1504239482343,    
+        'point_value': 10,
+        'send_name': 'Ronald Marshall',
+        'note': 'Tacos make me happy.'
+      },
+      {
+        'send_id': 8,
+        'receive_id': 3,
+        'group_id': 1,
+        'created_time': 1504239487190,
+        'point_value': 10,
+        'send_name': 'Paavo Marsicek',
+        'note': 'I did not think we would ever get finished with testing.'  
+      }
+    ]
+  }
+];
