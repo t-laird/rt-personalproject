0adf458b752c9477443e44f2827f82f72b971f4a
Rob Morgan <rmorgan323@gmail.com>
2018-01-02 22:47:22 -0700

    groupdata and userdata tests

--- src/Components/UserData/UserData.test.js
+++ src/Components/UserData/UserData.test.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { UserData, mapStateToProps } from './UserData';
+import { shallow } from 'enzyme';
+import { mockUserData } from '../../mockData/mockUserData';
 
+describe('user data tests', () => {
+  it('should match the snapshot', () => {
+    const renderedApp = shallow(<UserData user={mockUserData} />)
+
+    expect(renderedApp).toMatchSnapshot();
+  })
+})
+
+describe('mapStateToProps tests', () => {
+  it('should pull user from store', () => {
+    const mockStore = { mockUserData };
+    const result = mapStateToProps(mockStore);
+
+    expect(result.User).toEqual(mockStore.User);
+  })
+})
