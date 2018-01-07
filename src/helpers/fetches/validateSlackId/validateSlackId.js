b9a964699e6004487c24c0d3ea2138bab786e0ca
Rob Morgan <rmorgan323@gmail.com>
2018-01-07 10:37:26 -0700

    add mobile styles for group component

--- src/helpers/fetches/validateSlackId/validateSlackId.js
+++ src/helpers/fetches/validateSlackId/validateSlackId.js
@@ -2,7 +2,7 @@ import getKeyFromLS from '../../getKeyFromLS';
 
 const validateSlackId = async (userId) => {
 
-  const validateSlackResponse = await fetch('http://localhost:3000/api/v1/slack', {
+  const validateSlackResponse = await fetch('http://e1d037ce.ngrok.io/api/v1/slack', {
     method: 'POST',
     headers: {
       "x-token": getKeyFromLS(),
