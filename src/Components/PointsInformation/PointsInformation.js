953f00e0241cdf81b2b6d24f577344644e2afd1c
Thomas Laird <t6r6l5@gmail.com>
2018-01-16 14:12:11 -0700

    move styling to points information component

--- src/Components/PointsInformation/PointsInformation.js
+++ src/Components/PointsInformation/PointsInformation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import './PointsInformation.css';
 
 class PointsInformation extends Component {
   getReceivedPoints = () => {
